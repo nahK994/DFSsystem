674db1468613e1a06aa6f919acfd0f139ec7bfd0
nahK994 <nkskl6@gmail.com>
2024-08-02 10:03:49 +0600

    fix bug in test tcp transport

--- p2p/tcp_transport_test.go
+++ p2p/tcp_transport_test.go
@@ -9,6 +9,6 @@ import (
 func TestTCPTransport(t *testing.T) {
 	listenAddr := ":4000"
 	tr := NewTCPTransport(listenAddr)
-	assert.Equal(t, tr.ListenAndAccept(), listenAddr)
+	assert.Equal(t, tr.listenAddress, listenAddr)
 	assert.Nil(t, tr.ListenAndAccept())
 }
